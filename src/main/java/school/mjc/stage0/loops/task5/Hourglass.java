aafcf4fab1fd189a39c6a3889f7e98037ad4471f
medjik <med.akhmetov@gmail.com>
2023-08-29 11:36:45 +0600

    first attemtp

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,38 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+            for(int i=1;i<=height/2;i++){
+                for(int j=1;j<=height;j++){
+                    if(i==1){
+                        System.out.print("8");
+                        continue;
+                    }
+                    if(i<=height/2){
+                        if(j>=i&&j<=(height-i+1)){
+                            System.out.print("8");
+                        }
+                        else{
+                            System.out.print(" ");
+                        }
+                    }
+                }
+                System.out.println();
+            }
 
+            for(int i=height;i>=(height/2)+1;i--){
+                for(int j=1;j<=height;j++){
+                    if(i==(height/2)+1){
+                        System.out.print("8");
+                        continue;
+                    }
+                    if(j>=i-(height/2)&&j<=height-i){
+                        System.out.print("8");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
     }
 }
